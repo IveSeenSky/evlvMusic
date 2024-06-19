df3c579ebe48308f39e21fd99a1827aea47f33aa
evolventa <brawlme27@gmail.com>
2024-06-19 20:34:20 +0300

    Release version
    Removing from my old github acc tho

---
+++ app/src/main/java/com/example/myapplication/models/CategoryModel.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.models
+data class CategoryModel(
+    val name : String,
+    val coverUrl : String,
+    var songs : List<String>
+) {
+    constructor() : this("","", listOf())
+}
